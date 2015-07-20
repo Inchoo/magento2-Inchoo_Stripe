1d7fca9220dcf972428a40bc917c86936a8ff660
Stjepan Udovicic <udovicic.stjepan@gmail.com>
2015-07-20 23:26:09 +0200

    Cleanup of comments

--- Model/Source/Cctype.php
+++ Model/Source/Cctype.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Payment CC Types Source Model
  *
--- view/frontend/web/js/view/payment/stripe-payments.js
+++ view/frontend/web/js/view/payment/stripe-payments.js
@@ -1,6 +1,11 @@
 /**
- * Copyright © 2015 Inchoo. All rights reserved.
- * See COPYING.txt for license details.
+ * Inchoo_Stripe Magento JS component
+ *
+ * @category    Inchoo
+ * @package     Inchoo_Stripe
+ * @author      Ivan Weiler <ivan.weiler@inchoo.net> & Stjepan Udovičić <stjepan.udovicic@inchoo.net>
+ * @copyright   Inchoo (http://inchoo.net)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 /*browser:true*/
 /*global define*/
